28ebfcfdd61db0a0274c457de9a2da92ee9d359c
Cas.vanRijbroek <casvanrijbroek@hotmail.com>
2020-06-11 09:30:19 -0700

    attributes bug fix

--- api/wijnen_api.py
+++ api/wijnen_api.py
@@ -67,7 +67,7 @@ def get_attributes():
     if not verify_api_key(f["api_key"]):
         return jsonify({"error_code": "401, Unauthorized"}), 401
 
-    client = create_wijnen_client(f["additional_parameters"])
+    client = create_wijnen_client()
 
     return jsonify(client.get_attributes())
 
